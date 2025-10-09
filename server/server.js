48fcff6fce56905884a09c1e7e4215250388a35a
Your Name <you@example.com>
2025-10-09 19:49:49 +0530

    update   code

--- server/server.js
+++ server/server.js
@@ -6,11 +6,15 @@ const crypto = require('crypto');
 const connectDB = require("./config/db");
 const Transaction = require("./models/Transaction-model ");
 
+
+const FRONTEND_URL = process.env.FRONTEND_URL || 'http://localhost:5173' || 'http://localhost:5174';
+
+
 // Initialize express app
 const app = express();
 
 const corsOptions = {
-  origin: ['http://localhost:5173', 'http://localhost:5174'], 
+  origin: FRONTEND_URL,
   credentials: true, // Allow credentials (cookies)
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS', 'PATCH'],
   allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With']
