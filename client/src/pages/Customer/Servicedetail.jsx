bdb7bfb5529909d8a6791227be63b917da3c981b
Your Name <you@example.com>
2025-12-29 21:13:33 +0530

    Add code

--- client/src/pages/Customer/Servicedetail.jsx
+++ client/src/pages/Customer/Servicedetail.jsx
@@ -720,7 +720,7 @@ const ServiceDetailPage = () => {
 
                           <div className="flex justify-between items-center py-3">
                             <span className="font-medium text-gray-600">Total Images</span>
-                            <span className="text-gray-800 font-semibold bg-teal-100 text-teal-800 px-3 py-1 rounded-full border border-teal-200">
+                            <span className="text-gray-800 font-semibold bg-teal-100 px-3 py-1 rounded-full border border-teal-200">
                               {allImages.length} photos
                             </span>
                           </div>
