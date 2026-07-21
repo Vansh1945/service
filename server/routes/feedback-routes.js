fc979a80d7df5ba9735f56c602be82e9008eaea9
Your Name <you@example.com>
2026-07-21 18:50:17 +0530

    feedback system routes and server configuration

--- server/routes/feedback-routes.js
+++ server/routes/feedback-routes.js
@@ -12,7 +12,7 @@ const {
   deleteFeedbackAdmin,
   getServiceFeedbacks,
   toggleFeedbackApproval
-} = require('../controllers/Feedback-controller');
+} = require('../controllers/feedback-controller');
 const { userAuthMiddleware } = require('../middlewares/User-middleware');
 const { providerAuthMiddleware } = require('../middlewares/Provider-middleware');
 const adminAuthMiddleware = require('../middlewares/Admin-middleware');
